6694a397f9ba7e344ea5a997c9c6884021a4814d
Mahder Desta <mahderds2013@gmail.com>
2024-11-21 17:44:16 +0300

    fix: footer

--- src/app/components/footer.tsx
+++ src/app/components/footer.tsx
@@ -16,13 +16,13 @@ export default function Footer() {
   return (
     <footer className="py-8 bg-slate-800 dark:bg-gray-900">
       <div className="container">
-        <div className="grid md:grid-cols-12 items-center">
+        <div className="flex flex-col justify-center space-y-4 items-center">
           <div className="md:col-span-3">
             <Link href="#" className="logo-footer">
               <Image
                 src="/images/logo-light.png"
-                width={73}
-                height={21}
+                width={121}
+                height={27}
                 className="md:ms-0 mx-auto"
                 alt=""
               />
@@ -43,91 +43,6 @@ export default function Footer() {
                 .
               </p>
             </div>
-          </div>
-
-          <div className="md:col-span-4 md:mt-0 mt-8">
-            <ul className="list-none foot-icon ltr:md:text-right rtl:md:text-left text-center">
-              <li className="inline">
-                <Link
-                  href="https://1.envato.market/appever-next"
-                  target="_blank"
-                  className="size-8 inline-flex items-center justify-center tracking-wide align-middle text-base border border-gray-700 hover:border-primary rounded-md text-slate-300 hover:text-white hover:bg-primary"
-                >
-                  <FiShoppingCart
-                    className="h-4 w-4 align-middle"
-                    title="Buy Now"
-                  />
-                </Link>
-              </li>
-
-              <li className="inline">
-                <Link
-                  href="https://dribbble.com/shreethemes"
-                  target="_blank"
-                  className="size-8 inline-flex items-center justify-center tracking-wide align-middle text-base border border-gray-700 hover:border-primary rounded-md text-slate-300 hover:text-white hover:bg-primary"
-                >
-                  <FiDribbble
-                    className="h-4 w-4 align-middle"
-                    title="dribbble"
-                  />
-                </Link>
-              </li>
-
-              <li className="inline">
-                <Link
-                  href="http://linkedin.com/company/shreethemes"
-                  target="_blank"
-                  className="size-8 inline-flex items-center justify-center tracking-wide align-middle text-base border border-gray-700 hover:border-primary rounded-md text-slate-300 hover:text-white hover:bg-primary"
-                >
-                  <FiLinkedin
-                    className="h-4 w-4 align-middle"
-                    title="Linkedin"
-                  />
-                </Link>
-              </li>
-
-              <li className="inline">
-                <Link
-                  href="https://www.facebook.com/shreethemes"
-                  target="_blank"
-                  className="size-8 inline-flex items-center justify-center tracking-wide align-middle text-base border border-gray-700 hover:border-primary rounded-md text-slate-300 hover:text-white hover:bg-primary"
-                >
-                  <FiFacebook
-                    className="h-4 w-4 align-middle"
-                    title="facebook"
-                  />
-                </Link>
-              </li>
-
-              <li className="inline">
-                <Link
-                  href="https://www.instagram.com/shreethemes/"
-                  target="_blank"
-                  className="size-8 inline-flex items-center justify-center tracking-wide align-middle text-base border border-gray-700 hover:border-primary rounded-md text-slate-300 hover:text-white hover:bg-primary"
-                >
-                  <FiInstagram className="h-4 w-4 align-middle" />
-                </Link>
-              </li>
-
-              <li className="inline">
-                <Link
-                  href="https://twitter.com/shreethemes"
-                  target="_blank"
-                  className="size-8 inline-flex items-center justify-center tracking-wide align-middle text-base border border-gray-700 hover:border-primary rounded-md text-slate-300 hover:text-white hover:bg-primary"
-                >
-                  <FiTwitter className="h-4 w-4 align-middle" title="twitter" />
-                </Link>
-              </li>
-
-              <li className="inline">
-                <Link
-                  href="mailto:support@shreethemes.in"
-                  className="size-8 inline-flex items-center justify-center tracking-wide align-middle text-base border border-gray-700 hover:border-primary rounded-md text-slate-300 hover:text-white hover:bg-primary"
-                >
-                  <FiMail className="h-4 w-4 align-middle" title="email" />
-                </Link>
-              </li>
-            </ul>
           </div>
         </div>
       </div>
